a8e1a714985f219be651ce6c9c9ff3b73c8b683f
tranthuyminhthu <tranthuyminhthu0611>
2024-12-27 22:03:02 +0700

    feat: init source

---
+++ src/test/java/com/example/kltn_vu_thu/KltnVuThuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kltn_vu_thu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnVuThuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
